59663c4b051aae5efcf90f7b299dde47ec8b00fa
Alqaf-Gautama <alqafgautama03@gmail.com>
2022-01-15 00:24:13 +0800

    tambah data base

---
+++ skripsi.sql
@@ -0,0 +1,267 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 14 Jan 2022 pada 17.19
+-- Versi server: 10.1.36-MariaDB
+-- Versi PHP: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `skripsi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id`, `nama`, `username`, `password`) VALUES
+(1, 'Admin', 'admin', '$2y$10$CPWbTBQTNQwxMfbEq0.BiuPiyH3/gWW8YY3q4ME4qEBpKfYk0HCjS');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang_masuk`
+--
+
+CREATE TABLE `barang_masuk` (
+  `id` int(11) NOT NULL,
+  `pupuk_id` int(11) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `jumlah_masuk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `barang_masuk`
+--
+
+INSERT INTO `barang_masuk` (`id`, `pupuk_id`, `tanggal_masuk`, `jumlah_masuk`) VALUES
+(3, 1, '2022-04-15', 150),
+(4, 2, '2022-01-02', 45);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jatah`
+--
+
+CREATE TABLE `jatah` (
+  `id` int(11) NOT NULL,
+  `petani_id` int(11) NOT NULL,
+  `pupuk_id` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `jatah`
+--
+
+INSERT INTO `jatah` (`id`, `petani_id`, `pupuk_id`, `jumlah`) VALUES
+(3, 4, 1, 23),
+(4, 4, 2, 19),
+(5, 6, 1, 23),
+(8, 7, 2, 12),
+(9, 7, 1, 23);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kelompok`
+--
+
+CREATE TABLE `kelompok` (
+  `id` int(11) NOT NULL,
+  `nama_kelompok` varchar(255) NOT NULL,
+  `jumlah_anggota` int(11) NOT NULL,
+  `ket` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `kelompok`
+--
+
+INSERT INTO `kelompok` (`id`, `nama_kelompok`, `jumlah_anggota`, `ket`) VALUES
+(2, 'wanasari', 10, 'aktif'),
+(3, 'tes', 11, 'aktif');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `permintaan`
+--
+
+CREATE TABLE `permintaan` (
+  `id` int(11) NOT NULL,
+  `tgl_permintaan` date NOT NULL,
+  `pupuk_id` int(11) NOT NULL,
+  `petani_id` int(11) NOT NULL,
+  `jatah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `petani`
+--
+
+CREATE TABLE `petani` (
+  `id` int(11) NOT NULL,
+  `kelompok_id` int(11) NOT NULL,
+  `nik` varchar(50) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `petani`
+--
+
+INSERT INTO `petani` (`id`, `kelompok_id`, `nik`, `nama`) VALUES
+(4, 3, '133456', 'alqaf'),
+(6, 3, '64663', 'gau'),
+(7, 2, '12345678912', 'gautama');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pupuk`
+--
+
+CREATE TABLE `pupuk` (
+  `id` int(11) NOT NULL,
+  `nama_pupuk` varchar(255) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `harga` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pupuk`
+--
+
+INSERT INTO `pupuk` (`id`, `nama_pupuk`, `stock`, `harga`) VALUES
+(1, 'NPK', 123, 100),
+(2, 'UREA', 12, 1000),
+(3, 'ZA', 1220, 100),
+(4, 'SP-36', 120, 100),
+(5, 'ORGANIK GRANUL', 123, 100),
+(6, 'ORGANIK CAIR', 123, 100);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `jatah`
+--
+ALTER TABLE `jatah`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `kelompok`
+--
+ALTER TABLE `kelompok`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `permintaan`
+--
+ALTER TABLE `permintaan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `petani`
+--
+ALTER TABLE `petani`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pupuk`
+--
+ALTER TABLE `pupuk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `jatah`
+--
+ALTER TABLE `jatah`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `kelompok`
+--
+ALTER TABLE `kelompok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `permintaan`
+--
+ALTER TABLE `permintaan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `petani`
+--
+ALTER TABLE `petani`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `pupuk`
+--
+ALTER TABLE `pupuk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
